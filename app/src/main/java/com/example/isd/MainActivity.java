a0d41a242a62d3e8dd4175bd167e5761dc3e67b8
Konstantin Belousov <belolnik21@gmail.com>
2023-10-27 17:57:22 +0500

    SeekBar

--- app/src/main/java/com/example/isd/MainActivity.java
+++ app/src/main/java/com/example/isd/MainActivity.java
@@ -13,15 +13,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        TextView timeTextView = findViewById(R.id.timeTextView);
-        TimePicker timePicker = this.findViewById(R.id.timePicker);
-
-        timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
-            @Override
-            public void onTimeChanged(TimePicker view, int i, int i1) {
-                timeTextView.setText("Время: " + i + ":"+i1);
-            }
-        });
     }
 }
