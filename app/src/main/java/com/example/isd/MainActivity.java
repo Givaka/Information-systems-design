6cdf3539d106cff5957269c631e555c912cbd4f4
Konstantin Belousov <belolnik21@gmail.com>
2023-10-27 17:54:23 +0500

    RadioGroup setOnCheckedChangeListener

--- app/src/main/java/com/example/isd/MainActivity.java
+++ app/src/main/java/com/example/isd/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
@@ -16,15 +17,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void onRadioButtonClicked(View view) {
-        boolean checked = ((RadioButton) view).isChecked();
-        TextView selection = findViewById(R.id.selection);
+        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radios);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                TextView selection = findViewById(R.id.selection);
 
-        if (checked) {
-            if (view.getId() == R.id.java) selection.setText("Выбрана Java");
-            if (view.getId() == R.id.kotlin) selection.setText("Выбран Kotlin");
-        }
+                if (i == R.id.java) selection.setText("Выбрана Java");
+                if (i == R.id.kotlin) selection.setText("Выбран Kotlin");
+
+            }
+        });
     }
 }
