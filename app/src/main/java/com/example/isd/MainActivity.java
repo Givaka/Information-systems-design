5b026eb676cec44e5828ac415d28eb791af29a77
Konstantin Belousov <belolnik21@gmail.com>
2023-10-27 15:45:42 +0500

    Text View

--- app/src/main/java/com/example/isd/MainActivity.java
+++ app/src/main/java/com/example/isd/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.isd;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.second_layout);
+
+        TextView textView =findViewById(R.id.header);
+        textView.setText("Hello from Java!");
     }
 }
