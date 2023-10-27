7cadb207bf16ff2b670a81555e4c843aecadf2a6
Konstantin Belousov <belolnik21@gmail.com>
2023-10-27 17:50:26 +0500

    Checkbox

--- app/src/main/java/com/example/isd/MainActivity.java
+++ app/src/main/java/com/example/isd/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.isd;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onCheckBoxClick(View view){
+        CheckBox checkBox = (CheckBox) view;
+        TextView selection = findViewById(R.id.selection);
+
+        if(checkBox.isChecked()){
+            selection.setText("Включено");
+            checkBox.setText("Выключить");
+        } else {
+            selection.setText("Выключено");
+            checkBox.setText("Включить");
+        }
     }
 }
