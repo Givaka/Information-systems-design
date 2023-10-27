ab99b7e1a29d8c529bc4818f92aa7c7c6559726f
Konstantin Belousov <belolnik21@gmail.com>
2023-10-27 17:46:45 +0500

    Snackbar setAction

--- app/src/main/java/com/example/isd/MainActivity.java
+++ app/src/main/java/com/example/isd/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 
@@ -16,6 +17,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClick(View view){
-        Snackbar.make(view, "Hello Android!", Snackbar.LENGTH_LONG).show();
+        Snackbar snackbar = Snackbar.make(view, "Hello Android!", Snackbar.LENGTH_LONG);
+        snackbar.setAction("Next...", new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Next clicked!", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+        snackbar.show();
     }
 }
