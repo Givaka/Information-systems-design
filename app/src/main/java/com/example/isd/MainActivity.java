9c2298712141b17743ecd02a8ec6070bed3ebc2d
Konstantin Belousov <belolnik21@gmail.com>
2023-10-27 17:34:26 +0500

    Вложенные layout

--- app/src/main/java/com/example/isd/MainActivity.java
+++ app/src/main/java/com/example/isd/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.isd;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    int clicks = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View plusBtnView    = findViewById(R.id.plus_button);
+        View minusBtnView   = findViewById(R.id.minus_button);
+
+        TextView clicksText = findViewById(R.id.clicksText);
+
+        Button plusBtn  = plusBtnView.findViewById(R.id.clickBtn);
+        Button minusBtn = minusBtnView.findViewById(R.id.clickBtn);
+
+        plusBtn.setText("+");
+        minusBtn.setText("-");
+
+        plusBtn.setOnClickListener(v -> {clicksText.setText(++clicks + " Clicks");});
+        minusBtn.setOnClickListener(v -> {clicksText.setText(--clicks + " Clicks");});
     }
 }
