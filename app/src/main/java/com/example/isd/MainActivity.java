13716170f5cb325752c537eb7653f4a4704e8ab4
Konstantin Belousov <belolnik21@gmail.com>
2023-10-27 17:55:28 +0500

    DatePicker

--- app/src/main/java/com/example/isd/MainActivity.java
+++ app/src/main/java/com/example/isd/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.isd;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView dateTextView = findViewById(R.id.dateTextView);
+        DatePicker datePicker = this.findViewById(R.id.datePicker);
+
+        datePicker.init(2020, 02, 01, new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker view, int i, int i1, int i2) {
+                dateTextView.setText("Дата: " + view.getDayOfMonth() + "/" + (view.getMonth() + 1) + "/" + view.getYear());
+            }
+        });
     }
 }
