039101a6ccf728649885d1f8564231b264eb505b
Konstantin Belousov <belolnik21@gmail.com>
2023-10-27 17:42:22 +0500

    Button

--- app/src/main/java/com/example/isd/MainActivity.java
+++ app/src/main/java/com/example/isd/MainActivity.java
@@ -1,13 +1,9 @@
 package com.example.isd;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -16,21 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
+    public void sendMessage(View view){
+        TextView textView = findViewById(R.id.textView);
         EditText editText = findViewById(R.id.editText);
 
-        editText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                TextView textView = findViewById(R.id.textView);
-                textView.setText(charSequence);
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {}
-        });
+        textView.setText("Добро пожаловать, " + editText.getText());
     }
 }
