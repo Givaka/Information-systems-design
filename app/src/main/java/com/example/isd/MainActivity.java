e9203f95873693cca13c8c394048cf03562ca5e3
Konstantin Belousov <belolnik21@gmail.com>
2023-11-09 13:00:17 +0500

    Воспроизведение аудио

--- app/src/main/java/com/example/isd/MainActivity.java
+++ app/src/main/java/com/example/isd/MainActivity.java
@@ -2,33 +2,111 @@ package com.example.isd;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.MediaController;
+import android.widget.SeekBar;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
-    VideoView videoPlayer;
+    MediaPlayer mediaPlayer;
+    Button playButton, pauseButton, stopButton;
+    SeekBar volumeControl;
+    AudioManager audioManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        videoPlayer = findViewById(R.id.videoPlayer);
-        videoPlayer.setVideoPath("http://techslides.com/demos/sample-videos/small.mp4");
+        mediaPlayer = MediaPlayer.create(this, R.raw.megalovania);
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                stopPlay();
+            }
+        });
+
+        playButton = findViewById(R.id.playButton);
+        pauseButton = findViewById(R.id.pauseButton);
+        stopButton = findViewById(R.id.stopButton);
+
+        audioManager    = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+        int maxVolume   = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int curValue    = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+
+        volumeControl   = findViewById(R.id.volumeControl);
+        volumeControl.setMax(maxVolume);
+        volumeControl.setProgress(curValue);
+        volumeControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+        pauseButton.setEnabled(false);
+        stopButton.setEnabled(false);
+
+    }
+
+    private void stopPlay() {
+        mediaPlayer.stop();
+
+        pauseButton.setEnabled(false);
+        stopButton.setEnabled(false);
+
+        try {
+            mediaPlayer.prepare();
+            mediaPlayer.seekTo(0);
+            playButton.setEnabled(true);
+        } catch (Throwable t){
+            Toast.makeText(this, t.getMessage(), Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void play(View view){
-        videoPlayer.start();
+        mediaPlayer.start();
+
+        playButton.setEnabled(false);
+        pauseButton.setEnabled(true);
+        stopButton.setEnabled(true);
     }
     public void pause(View view){
-        videoPlayer.pause();
+        mediaPlayer.pause();
+
+        playButton.setEnabled(true);
+        pauseButton.setEnabled(false);
+        stopButton.setEnabled(true);
     }
     public void stop(View view){
-        videoPlayer.stopPlayback();
-        videoPlayer.resume();
+        stopPlay();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mediaPlayer.isPlaying()) {
+            stopPlay();
+        }
     }
 }
