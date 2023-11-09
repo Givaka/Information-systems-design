087af556dba937f2bf28ee9ae91c4410e8d91b73
Konstantin Belousov <belolnik21@gmail.com>
2023-11-09 11:48:17 +0500

    Передача данных между Activity

--- app/src/main/java/com/example/isd/MainActivity.java
+++ app/src/main/java/com/example/isd/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.isd;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view){
+        EditText nameText       = findViewById(R.id.name);
+        EditText companyText    = findViewById(R.id.company);
+        EditText ageText        = findViewById(R.id.age);
+
+        Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra("name",     nameText.getText().toString());
+        intent.putExtra("company",  companyText.getText().toString());
+        intent.putExtra("age",      Integer.parseInt(ageText.getText().toString()));
+
+        startActivity(intent);
     }
 }
