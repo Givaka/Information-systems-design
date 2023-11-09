8be4d779f3a1344ca71d509e29cb793eb99a90a8
Konstantin Belousov <belolnik21@gmail.com>
2023-11-09 12:30:18 +0500

    Работа с видео

--- app/src/main/java/com/example/isd/MainActivity.java
+++ app/src/main/java/com/example/isd/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.isd;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+
+    VideoView videoPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        videoPlayer = findViewById(R.id.videoPlayer);
+        String uriString = "android.resource://" + getPackageName() + "/" + R.raw.ngcyu;
+        Uri videoUri = Uri.parse(uriString);
+        videoPlayer.setVideoURI(videoUri);
+    }
+
+    public void play(View view){
+        videoPlayer.start();
+    }
+    public void pause(View view){
+        videoPlayer.pause();
+    }
+    public void stop(View view){
+        videoPlayer.stopPlayback();
+        videoPlayer.resume();
     }
 }
