d678e5b58a90d603c885e42ce1950e7367cb08e5
Konstantin Belousov <belolnik21@gmail.com>
2023-11-15 13:57:36 +0500

    WebView

--- app/src/main/java/com/example/isd/MainActivity.java
+++ app/src/main/java/com/example/isd/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.isd;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        WebView browser = new WebView(this);
+        setContentView(browser);
+        browser.loadUrl("https://youtu.be/dQw4w9WgXcQ?si=vFDaOnDASjCNKqC0");
     }
 }
