c244c489165f967bf2af95fcf97cb23e954d5799
Konstantin Belousov <belolnik21@gmail.com>
2023-11-18 07:00:12 +0500

    Обработка нажатий в меню

--- app/src/main/java/com/example/isd/MainActivity.java
+++ app/src/main/java/com/example/isd/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.isd;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +20,17 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        TextView headerView = findViewById(R.id.selectedMenuItem);
+
+        if (id == R.id.action_settings) headerView.setText("Настройки");
+        if (id == R.id.open_settings) headerView.setText("Открыть");
+        if (id == R.id.save_settings) headerView.setText("Сохранить");
+
+        return super.onOptionsItemSelected(item);
     }
 }
