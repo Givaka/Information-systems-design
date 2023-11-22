36b42d22c4ddbc321e5f4ddd40991129937e930e
Konstantin Belousov <belolnik21@gmail.com>
2023-11-22 18:41:52 +0500

    Использование существующей БД SQLite

--- app/src/main/java/com/example/isd/DatabaseHelper.java
+++ app/src/main/java/com/example/isd/DatabaseHelper.java
@@ -1,12 +1,20 @@
 package com.example.isd;
 
 import android.content.Context;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
-
-    private static final String DATABASE_NAME = "userstore.db";
+    private static String DB_PATH;
+    private static final String DB_NAME = "db.db";
     private static final  int SHEMA = 1;
     static final String TABLE = "users";
 
@@ -14,19 +22,40 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String COLUMN_NAME = "name";
     public static final String COLUMN_YEAR = "year";
 
+    private Context myContext;
+
     public DatabaseHelper(Context context){
-        super(context, DATABASE_NAME, null, SHEMA);
+        super(context, DB_NAME, null, SHEMA);
+        this.myContext = context;
+
+        DB_PATH = context.getFilesDir().getPath() + DB_NAME;
     }
 
     @Override
-    public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE users ("+ COLUMN_ID +" INTEGER PRIMARY KEY AUTOINCREMENT," + COLUMN_NAME + " TEXT, "+ COLUMN_YEAR+" INTEGER)");
-        db.execSQL("INSERT INTO "+ TABLE +" (" + COLUMN_NAME + ", " + COLUMN_YEAR + ") VALUES ('Том Смит', 1981);");
-    }
+    public void onCreate(SQLiteDatabase db) {    }
 
     @Override
-    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE);
-        onCreate(db);
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {    }
+
+    void create_db(){
+        File file = new File(DB_PATH);
+
+        if (!file.exists()) {
+            try (InputStream myInput    = myContext.getAssets().open(DB_NAME);
+                OutputStream myOutput   = new FileOutputStream(DB_PATH)) {
+
+                byte[] buffer = new byte[1024];
+                int length;
+
+                while ((length = myInput.read(buffer)) > 0) myOutput.write(buffer, 0 ,length);
+                myOutput.flush();
+
+            } catch (IOException ex){
+                Log.d("DatabaseHelper", ex.getMessage());
+            }
+        }
+    }
+    public SQLiteDatabase open() throws SQLException{
+        return SQLiteDatabase.openDatabase(DB_PATH, null, SQLiteDatabase.OPEN_READWRITE);
     }
 }
--- app/src/main/java/com/example/isd/UserActivity.java
+++ app/src/main/java/com/example/isd/UserActivity.java
@@ -25,48 +25,52 @@ public class UserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
-
-        nameBox     = findViewById(R.id.name);
-        yearBox     = findViewById(R.id.year);
-        delButton   = findViewById(R.id.deleteButton);
-        saveButton  = findViewById(R.id.saveButton);
-
-        sqlHelper   = new DatabaseHelper(this);
-        db          = sqlHelper.getWritableDatabase();
-
+        nameBox = findViewById(R.id.name);
+        yearBox = findViewById(R.id.year);
+        delButton = findViewById(R.id.deleteButton);
+        saveButton = findViewById(R.id.saveButton);
+        sqlHelper = new DatabaseHelper(this);
+        db = sqlHelper.open();
         Bundle extras = getIntent().getExtras();
-        if (extras != null) userId = extras.getLong("id");
-        if (userId > 0 ){
-            userCursor = db.rawQuery("select * from " + DatabaseHelper.TABLE + " where " + DatabaseHelper.COLUMN_ID + "=?",
-                    new String[]{String.valueOf(userId)});
+        if (extras != null) {
+            userId = extras.getLong("id");
+        }
+        // если 0, то добавление
+        if (userId > 0) {
+            // получаем элемент по id из бд
+            userCursor = db.rawQuery("select * from " + DatabaseHelper.TABLE + " where " + DatabaseHelper.COLUMN_ID + "=?", new String[]{String.valueOf(userId)});
             userCursor.moveToFirst();
             nameBox.setText(userCursor.getString(1));
             yearBox.setText(String.valueOf(userCursor.getInt(2)));
             userCursor.close();
         } else {
+            // скрываем кнопку удаления
             delButton.setVisibility(View.GONE);
         }
     }
-
     public void save(View view){
         ContentValues cv = new ContentValues();
-
-        cv.put(DatabaseHelper.COLUMN_NAME, nameBox.getText().toString());
-        cv.put(DatabaseHelper.COLUMN_YEAR, Integer.parseInt(yearBox.getText().toString()));
-
-        if (userId > 0 ) db.update(DatabaseHelper.TABLE, cv, DatabaseHelper.COLUMN_ID + "=" + userId, null);
-        else db.insert(DatabaseHelper.TABLE, null, cv);
-
-        goHome ();
-    }
-
-    public void delete (View view){
-        db.delete(DatabaseHelper.TABLE, "_id = ?", new String[]{String.valueOf(userId)});
+        cv.put(DatabaseHelper.COLUMN_NAME,
+                nameBox.getText().toString());
+        cv.put(DatabaseHelper.COLUMN_YEAR,
+                Integer.parseInt(yearBox.getText().toString()));
+        if (userId > 0) {
+            db.update(DatabaseHelper.TABLE, cv,
+                    DatabaseHelper.COLUMN_ID + "=" + userId, null);
+        } else {
+            db.insert(DatabaseHelper.TABLE, null, cv);
+        }
         goHome();
     }
-
+    public void delete(View view){
+        db.delete(DatabaseHelper.TABLE, "_id = ?",
+                new String[]{String.valueOf(userId)});
+        goHome();
+    }
     private void goHome(){
+        // закрываем подключение
         db.close();
+        // переход к главной activity
         Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
         startActivity(intent);
